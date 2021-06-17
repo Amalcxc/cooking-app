71a8480ff2076403396c0a25c7057e762b3adcd0
Amalcxc <amalcxirsi@gmail.com>
2021-06-18 00:52:14 +0300

    rspec

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'accepts a valid message' do
+    user1 = User.create(name: 'NewUser',username:'Newuser', email: 'user@example.com', password: '1234567', password_confirmation: '1234567')
+    idea1 = user1.ideas.create(idea: 'new idea created')
+    expect(idea1.valid?).to eq(true)
+  end
+
+  it 'does not accept a message from without an user' do
+    idea1 = Idea.create(idea: 'new idea created')
+    expect(idea1.valid?).to eq(false)
+  end
+
+  it 'does not accept a empty message' do
+    idea1 = Idea.create
+    expect(idea1.valid?).to eq(false)
+  end
+
+  it 'destroys a idea successfully' do
+    user1 = User.create(name: 'NewUser',username:'Newuser', email: 'user@example.com', password: '1234567', password_confirmation: '1234567')
+    idea1 = user1.ideas.create(idea: "Idea was successfully destroyed.")
+
+    expect { idea1.destroy }
+  end
 end
