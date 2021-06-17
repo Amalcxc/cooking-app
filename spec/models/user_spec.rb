b433cbdba8eb796dac2e38632f8d625d9695a53a
Amalcxc <amalcxirsi@gmail.com>
2021-06-17 23:45:00 +0300

    fix the box length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a valid user' do
+    user1 = User.create(username: 'andressaborges', name: 'John')
+    expect(user1.valid?).to eq(true)
+  end
+
+  it 'not allows the creation of a user without username' do
+    user1 = User.create(full_name: 'Andressa Borges')
+    expect(user1.valid?).to eq(false)
+  end
+
+  it 'not allows the creation of a user without Full Name' do
+    user1 = User.create(username: 'andressaborges')
+    expect(user1.valid?).to eq(false)
+  end
 end
